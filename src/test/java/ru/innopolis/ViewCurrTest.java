372b25cd5d194451985b22e82d2880ea59011bd2
darik17 <darik17@yandex.ru>
2017-06-27 20:21:22 +0300

    Добавил основной метод @Test

--- src/test/java/ru/innopolis/ViewCurrTest.java
+++ src/test/java/ru/innopolis/ViewCurrTest.java
@@ -1,19 +1,21 @@
 package ru.innopolis;
 
 import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -28,10 +30,19 @@ public class ViewCurrTest {
     private static String fileParam = "src\\main\\resources\\viewcurrtest.csv";
     private String currFrom;
     private String currIn;
+    private static Map<String, String> currName = new HashMap<>();
 
     public ViewCurrTest(String currFrom, String currIn) {
         this.currFrom = currFrom;
         this.currIn = currIn;
+    }
+
+    static {
+       currName.put("CHF","Швейцарский франк");
+       currName.put("EUR","Евро");
+       currName.put("GBP","Фунт стерлингов Соединенного Королевства");
+       currName.put("JPY","Японская иена");
+       currName.put("USD","Доллар США");
     }
 
     @Parameterized.Parameters
@@ -65,5 +76,88 @@ public class ViewCurrTest {
         driver.navigate().to("https://www.sberbank.ru/ru/quotes/converter");
     }
 
+    /**
+     * Тестирование появление графика изменения валюты к рублю в зависимости от выбранной валюты в полях "из" и "в"
+     */
+    @Test
+    public void viewTest() {
 
+        // Выбор валюты откуда конвертируется сумма
+        Actions actionsCurrFrom = new Actions(driver);
+        WebElement webElemMenuFrom = driver.findElement(By.cssSelector(".rates-aside__filter-block > div:nth-child(3) > div:nth-child(2) > div:nth-child(2)"));
+        actionsCurrFrom.moveToElement(webElemMenuFrom).click().build().perform();
+
+        String elemMenuFrom = null;
+
+        switch (currFrom) {
+            case "RUB":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[1]";
+                break;
+            case "CHF":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[2]";
+                break;
+            case "EUR":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[3]";
+                break;
+            case "GBP":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[4]";
+                break;
+            case "JPY":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[5]";
+                break;
+            case "USD":
+                elemMenuFrom = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[3]/div[2]/div/div/span[6]";
+                break;
+        }
+
+        WebElement webElemMenuFromChoise = driver.findElement(By.xpath(elemMenuFrom));
+        actionsCurrFrom.moveToElement(webElemMenuFromChoise).click().build().perform();
+
+
+        // Выбор валюты куда нужно конвертировать
+        Actions actionsCurrIn = new Actions(driver);
+        WebElement webElemMenuIn = driver.findElement(By.cssSelector(".rates-aside__filter-block > div:nth-child(4) > div:nth-child(2) > div:nth-child(2)"));
+        actionsCurrIn.moveToElement(webElemMenuIn).click().build().perform();
+
+        String elemMenuIn = null;
+
+        switch (currIn) {
+            case "RUB":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[1]";
+                break;
+            case "CHF":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[2]";
+                break;
+            case "EUR":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[3]";
+                break;
+            case "GBP":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[4]";
+                break;
+            case "JPY":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[5]";
+                break;
+            case "USD":
+                elemMenuIn = ".//*[@id='main']/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div[3]/div/div/aside/div/div/div/div[2]/div/div[1]/div[4]/div[2]/div/div/span[6]";
+                break;
+
+
+        }
+
+        WebElement webElemMenuInChose = driver.findElement(By.xpath(elemMenuIn));
+        actionsCurrIn.moveToElement(webElemMenuInChose).click().build().perform();
+
+
+        //Поиск  название валюты в элементе график валюты, выбранной в поле "из" и сравнение с названием получившегося графика
+        WebElement webGrafFrom = driver.findElement(By.cssSelector(".rates-details-graphs > div:nth-child(1) > h2:nth-child(4)"));
+        String textGrFrom = webGrafFrom.getText();
+        Assert.assertTrue(textGrFrom.equals(currName.get(currFrom)));
+
+
+        //Поиск  название валюты в элементе график валюты, выбранной в поле "в" и сравнение с названием получившегося графика
+        WebElement webGrafIn = driver.findElement(By.cssSelector(".rates-details-graphs > div:nth-child(2) > h2:nth-child(4)"));
+        String textGrIn = webGrafFrom.getText();
+        Assert.assertTrue(textGrIn.equals(currName.get(currIn)));
+
+    }
 }
